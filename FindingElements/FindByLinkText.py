178f2bebeb622ea99703aebdd0be32149410ec8d
Adam <abialasik94@gmail.com>
2019-09-04 14:40:21 +0200

    Finding elements classified to another python folder, changed readme.md.

--- FindingElements/FindByLinkText.py
+++ FindingElements/FindByLinkText.py
@@ -1,26 +1,23 @@
 from selenium import webdriver
 
 
-class FindByName():
+class FindByLinkText():
 
     def test(self):
         baseUrl = "https://learn.letskodeit.com/p/practice"
         driver = webdriver.Firefox(
             executable_path="/home/adam/Dokumenty/staż/kursy/selenium/geckodriver-v0.24.0-linux64/geckodriver")
         driver.get(baseUrl)
-        elementById = driver.find_element_by_id("name")
+
+        elementById = driver.find_element_by_id("Login")
 
         if elementById is not None:
             print("We found and element")
+        elementByLinkText = driver.find_element_by_partial_link_text("Pract")
 
-        elementByName = driver.find_element_by_name("show-hide")
-
-        if elementByName is not None:
+        if elementByLinkText is not None:
             print("We found an element by Name")
 
-        elementByClass = driver.find_element_by_class_name("btn-style")
-        if elementByClass is not None:
-            print("We found an element by Class")
 
 ff = FindByName()
 ff.test()
